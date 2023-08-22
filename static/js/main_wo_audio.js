c735a35ca67e396601fc8bc422d0194270266b0f
Jakob Hærvig <10219471+jakobhaervig@users.noreply.github.com>
2023-08-22 15:30:33 +0200

    Update

---
+++ static/js/main_wo_audio.js
@@ -0,0 +1,59 @@
+Reveal.initialize({
+    width: 1920, // 1460
+    height: 1080, // 1050
+    margin: 0.04,
+    minScale: 0.2,
+    maxScale: 2.0,
+    center: true,
+    slideNumber: slide => {return [ 'Topic '+Reveal.getIndices( slide ).h+','+Reveal.getIndices( slide ).v]},
+    history: true,
+    pdfSeparateFragments: false,
+    fragments: true,
+    fragmentInURL: true,
+    plugins: [
+        RevealMarkdown,
+        RevealMath.KaTeX,
+        //RevealAudioSlideshow,
+        RevealSearch,
+        RevealHighlight
+    ],
+    katex: {
+        version: 'latest',
+        delimiters: [{
+                left: '$$',
+                right: '$$',
+                display: true
+            },
+            {
+                left: '$',
+                right: '$',
+                display: false
+            },
+            {
+                left: '\\(',
+                right: '\\)',
+                display: false
+            },
+            {
+                left: '\\[',
+                right: '\\]',
+                display: true
+            }
+        ],
+        ignoredTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+    },
+    audio: {
+        autoplay: false,
+        playerStyle: 'position: fixed; bottom: 0.7em; left: 25%; width: 50%; height:2em; z-index: 33;',
+        defaultAudios: true,
+        prefix: 'media/audio/main/',
+        suffix: ".mp3",
+        defaultDuration: 600
+    }
+    })
+
+    window.addEventListener("load", function() {
+        revealDiv = document.querySelector("body div.reveal")
+        footer = document.getElementById("footer");
+        revealDiv.appendChild(footer);
+    });
